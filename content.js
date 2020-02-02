19a8357bf40d182684034ee9ad56cd9afd689f92
jasmaa <jasonmaa3955@gmail.com>
2020-02-02 17:23:51 -0500

    Implement rudimentary search

--- content.js
+++ content.js
@@ -18,18 +18,48 @@ for (post of postList.getElementsByClassName("post-preview-wrapper")) {
 	postData.push({ title: title, text: text });
 }
 
+const filterPosts = (keyword) => {
+	for (post of postList.getElementsByClassName("post-preview-wrapper")) {
+		const preview = post.getElementsByClassName("post-preview")[0];
+		const title = preview
+			.getElementsByClassName("post-title")[0]
+			.getElementsByTagName("H3")[0]
+			.innerHTML;
+		const text = preview
+			.getElementsByClassName("post-text-wrap")[0]
+			.getElementsByClassName("post-text")[0]
+			.innerHTML;
+
+		//put fuzzy check here
+		if (text.includes(keyword) || title.includes(keyword)) {
+			post.style.display = 'block';
+			post.classList.add('d-flex');
+		} else {
+			post.style.display = 'none';
+			post.classList.remove('d-flex');
+		}
+	}
+}
+
 
 // Re-route search
-
 const sidebar = document.body.getElementsByClassName("sidebar-category-wrap")[0];
 const searchBut = sidebar.getElementsByClassName("fa-search")[0].parentNode;
+searchBut.remove();
 
-const newSearchBut = searchBut.cloneNode(true);
-searchBut.parentNode.replaceChild(newSearchBut, searchBut);
-newSearchBut.addEventListener('click', e => {
-	alert("hi there");
-});
+const searchControl = document.createElement("DIV");
+searchControl.className = "sidebar-category-wrap d-flex align-items-center search-control";
 
-const test = document.createElement("p");
-test.innerHTML = "done!";
-sidebar.appendChild(test);
+const searchBar = document.createElement("INPUT");
+searchBar.className = "form-control";
+searchBar.addEventListener('input', e => {
+	console.log(searchBar.value);
+	filterPosts(searchBar.value);
+})
+searchControl.appendChild(searchBar);
+
+const searchIcon = document.createElement("I");
+searchIcon.className = "far fa-search";
+searchControl.appendChild(searchIcon);
+
+sidebar.after(searchControl);
